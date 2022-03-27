5980779a71b952bd7ea0da78dd1a16045e4b1975
bgalek <bgalek@users.noreply.github.com>
2022-03-27 23:00:15 +0000

    deploy: 718e1d3d81fea4a630492aa8086557fe4fae7e5c

--- _next/static/fAoMLuL7gQRgOHq6GZyRH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/982-67524700cabc8067.js","static/chunks/pages/index-b4490c7492fe27cc.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
